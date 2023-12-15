98517f964f0ed0328b9b19af06a7ee3f03c6f622
CJ1608 <128046995+CJ1608@users.noreply.github.com>
2023-12-15 22:52:19 +0000

    updating data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+
 class DataExtractor():
+    """
+    Method to extract data from database and return it as a pd.dataframe. 
+    """
     
     def read_rds_table(self, db_connector, table_name): 
         """
@@ -10,13 +14,10 @@ class DataExtractor():
             db_connector (database connection): database connection from init_db_engine method. 
             table_name (literal): name of table in database to be accessed. 
         Returns:
-            legacy_data_frame (pd.dataframe): dataframe of data to be cleaned. 
+            data_frame (pd.dataframe): dataframe of data to be cleaned. 
         """
         db_connected = db_connector.engine.connect()
         data_frame = pd.read_sql_table(table_name, db_connected)
         db_connected.close()
-        # print(data_frame)
         return data_frame
     
-    def read_local_csv():
-        pass
